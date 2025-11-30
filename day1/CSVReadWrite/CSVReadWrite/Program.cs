1dbf367e65064923acdead0f5e15141551b567ce
Adam Hulme <Adam.Hulme+cai@coxautoinc.com>
2025-11-30 21:34:44 +0000

    Pre-emptively add csv read/write for day 1

---
+++ day1/CSVReadWrite/CSVReadWrite/Program.cs
@@ -0,0 +1,14 @@
+﻿StreamReader sr = new StreamReader("D:\\projects\\adventofcode2025\\day1\\to-update.csv");
+var line = sr.ReadLine();
+var toWrite = new List<string>();
+while (line != null)
+{
+    Console.WriteLine(line);
+    toWrite.Add(line);
+    line = sr.ReadLine();
+}
+sr.Close();
+
+File.WriteAllLines("D:\\projects\\adventofcode2025\\day1\\updated.csv", toWrite);
+
+Console.ReadLine();
