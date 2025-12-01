d286d643684cce8fecf7597941d8f827eba0e40b
Adam Hulme <Adam.Hulme+cai@coxautoinc.com>
2025-12-01 08:16:49 +0000

    Tried recursion but still wrong

---
+++ day1/CSVReadWrite/CSVReadWrite/DialMover.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVReadWrite;
+public class DialMover
+{
+    public int DialPos { get; set; }
+    public int HitZeroCount { get; set; }
+
+    public DialMover()
+    {
+        DialPos = 50;
+        HitZeroCount = 0;
+    }
+
+    public void MoveDial(char direction, int amount)
+    {
+        if (direction == 'L')
+        {
+            if (DialPos == 0)
+            {
+                if (amount >= 100)
+                {
+                    HitZeroCount++;
+                    MoveDial(direction, amount - 100);
+                    return;
+                }
+                else
+                {
+                    DialPos = 100 - amount;
+                    return;
+                }
+            }
+
+            if (DialPos - amount > 0)
+                DialPos -= amount;
+            else if (DialPos - amount == 0)
+            {
+                DialPos = 0;
+                HitZeroCount++;
+            }
+            else
+            {
+                var moved = DialPos;
+                DialPos = 0;
+                HitZeroCount++;
+                MoveDial(direction, amount - moved);
+            }
+        }
+        else
+        {
+            if (DialPos + amount < 100)
+            {
+                DialPos += amount;
+            }
+            else
+            {
+                var moved = 100 - DialPos;
+                DialPos = 0;
+                HitZeroCount++;
+                MoveDial(direction, amount - moved);
+            }
+        }
+    }
+}
--- day1/CSVReadWrite/CSVReadWrite/Program.cs
+++ day1/CSVReadWrite/CSVReadWrite/Program.cs
@@ -1,14 +1,31 @@
-﻿StreamReader sr = new StreamReader("D:\\projects\\adventofcode2025\\day1\\to-update.csv");
-var line = sr.ReadLine();
-var toWrite = new List<string>();
-while (line != null)
+﻿using CSVReadWrite;
+
+class Program
 {
-    Console.WriteLine(line);
-    toWrite.Add(line);
-    line = sr.ReadLine();
+    public int zeroCount = 0;
+    static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader("D:\\projects\\adventofcode2025\\day1\\to-update.csv");
+        var line = sr.ReadLine();
+        var dial = new DialMover();
+        while (line != null)
+        {
+            var direction = line[0];
+            var amountToMove = int.Parse(line.Substring(1));
+
+            dial.MoveDial(direction, amountToMove);
+            if (dial.DialPos == 100)
+            {
+                dial.DialPos = 0;
+                dial.HitZeroCount++;
+            }
+            Console.WriteLine($"Current dial pos: {dial.DialPos}");
+            Console.WriteLine($"Finish zero: {dial.HitZeroCount}");
+            line = sr.ReadLine();
+        }
+        sr.Close();
+        Console.WriteLine(dial.HitZeroCount);
+
+        Console.ReadLine();
+    }
 }
-sr.Close();
-
-File.WriteAllLines("D:\\projects\\adventofcode2025\\day1\\updated.csv", toWrite);
-
-Console.ReadLine();
