23f42b9a70f0611efd0c910f48fab69e2ad01d36
Adam Hulme <Adam.Hulme+cai@coxautoinc.com>
2025-12-05 19:21:19 +0000

    Solve and tidy (finally)

--- day5/Program.cs
+++ day5/Program.cs
@@ -3,7 +3,7 @@
     public int zeroCount = 0;
     static void Main(string[] args)
     {
-        var filename = "C:\\projects\\aoc25\\day5\\in.txt";
+        var filename = "D:\\projects\\adventofcode2025\\day5\\in.txt";
         StreamReader sr = new StreamReader(filename);
         var line = sr.ReadLine();
         var ranges = new List<Range>();
@@ -12,51 +12,10 @@
             var range = line.Split('-');
             var low = long.Parse(range[0]);
             var high = long.Parse(range[1]);
-            ranges = ranges.Where(range => range.Low < low || range.High > high).ToList();
-            var lowOverlap = ranges.Any(range => low <= range.Low && range.Low <= high && high <= range.High);
-            if (lowOverlap)
-                high = ranges.Where(range => low <= range.Low && range.Low <= high && high <= range.High).Min(r => r.Low);
-            var highOverlap = ranges.Any(range =>  range.Low <= low && low <= range.High && high >= range.High);
-            if (highOverlap)
-                low = ranges.Where(range =>  range.Low <= low && low <= range.High && high >= range.High).Max(r => r.High);
-
-            if (ranges.Any(r => r.Low <= low && r.High >= high))
-            {
-                line = sr.ReadLine();
-                continue;
-            }
             ranges.Add(new Range(low,high));
             line = sr.ReadLine();
         }
-        // Merge ranges
-        // too low: 354143734113765 too high: 354143734113774 wrong 354143734113767
-        for (int i = 0; i < ranges.Count; i++)
-        {
-            var r1 = ranges[i];
-            if (ranges.Where(r2 => r1 != r2).Any(r2 => r2.Low <= r1.Low && r2.High >= r1.High))
-            {
-                Console.WriteLine("hi");
-                ranges.Remove(r1);
-                continue;
-            }
-
-            if (ranges.Any(r2 => r2.Low == r1.High))
-            {
-                Console.WriteLine("hi1");
-                var toRemove = ranges.First(r2 => r2.Low == r1.High);
-                r1.High = toRemove.High;
-                ranges.Remove(toRemove);
-            }
-
-            if (ranges.Any(r2 => r2.High == r1.Low))
-            {
-                Console.WriteLine("hi2");
-                var toRemove = ranges.First(r2 => r2.High == r1.Low);
-                r1.Low = toRemove.Low;
-                ranges.Remove(toRemove);
-            }
-        }
-
+        
         for (int i = 0; i < ranges.Count; i++)
         {
             var r1 = ranges[i];
@@ -64,24 +23,23 @@
             {
                 if (i == j) continue;
                 var r2 = ranges[j];
-                if (ranges.Where(r2 => r1 != r2).Any(r2 => r2.Low <= r1.Low && r2.High >= r1.High))
+                if (r2.Low <= r1.Low && r2.High >= r1.High)
                 {
-                    Console.WriteLine("hi");
                     ranges.Remove(r1);
-                    continue;
+                    i = -1;
+                    break;
                 }
 
                 if (r1.Low <= r2.Low && r2.Low <= r1.High)
                 {
-                    Console.WriteLine("hi3");
                     r1.High = Math.Max(r1.High, r2.High);
                     ranges.Remove(r2);
                     i = -1;
                     break;
                 }
+
                 if (r1.Low <= r2.High && r2.High <= r1.High)
                 {
-                    Console.WriteLine("hi4");
                     r1.Low = Math.Min(r1.Low, r2.Low);
                     ranges.Remove(r2);
                     i = -1;
