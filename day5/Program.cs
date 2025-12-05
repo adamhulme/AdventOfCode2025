beaf7abec4397c7c41b3e98e0d884008fdd3f08b
Adam Hulme <adamhulme4@gmail.com>
2025-12-05 18:14:44 +0000

    Commit latest (disgusting)

--- day5/Program.cs
+++ day5/Program.cs
@@ -3,7 +3,7 @@
     public int zeroCount = 0;
     static void Main(string[] args)
     {
-        var filename = "C:\\projects\\aoc25\\day5\\ex.txt";
+        var filename = "C:\\projects\\aoc25\\day5\\in.txt";
         StreamReader sr = new StreamReader(filename);
         var line = sr.ReadLine();
         var ranges = new List<Range>();
@@ -12,23 +12,24 @@
             var range = line.Split('-');
             var low = long.Parse(range[0]);
             var high = long.Parse(range[1]);
-            if (ranges.Any(r => r.Low < low && r.High > high))
+            ranges = ranges.Where(range => range.Low < low || range.High > high).ToList();
+            var lowOverlap = ranges.Any(range => low <= range.Low && range.Low <= high && high <= range.High);
+            if (lowOverlap)
+                high = ranges.Where(range => low <= range.Low && range.Low <= high && high <= range.High).Min(r => r.Low);
+            var highOverlap = ranges.Any(range =>  range.Low <= low && low <= range.High && high >= range.High);
+            if (highOverlap)
+                low = ranges.Where(range =>  range.Low <= low && low <= range.High && high >= range.High).Max(r => r.High);
+
+            if (ranges.Any(r => r.Low <= low && r.High >= high))
             {
-                Console.WriteLine("hi0");
                 line = sr.ReadLine();
                 continue;
             }
-            ranges = ranges.Where(range => range.Low < low || range.High > high).ToList();
-            var lowOverlap = ranges.Any(range => low < range.Low && range.Low < high && high < range.High);
-            if (lowOverlap)
-                high = ranges.Where(range => low < range.Low && range.Low < high && high < range.High).Min(r => r.Low);
-            var highOverlap = ranges.Any(range =>  range.Low < low && low < range.High && high > range.High);
-            if (highOverlap)
-                low = ranges.Where(range =>  range.Low < low && low < range.High && high > range.High).Max(r => r.High);
             ranges.Add(new Range(low,high));
             line = sr.ReadLine();
         }
         // Merge ranges
+        // too low: 354143734113765 too high: 354143734113774 wrong 354143734113767
         for (int i = 0; i < ranges.Count; i++)
         {
             var r1 = ranges[i];
@@ -55,9 +56,44 @@
                 ranges.Remove(toRemove);
             }
         }
-        ranges.ForEach(range => Console.WriteLine($"{range.Low}-{range.High}"));
-        //Console.WriteLine("Middle hit");
+
+        for (int i = 0; i < ranges.Count; i++)
+        {
+            var r1 = ranges[i];
+            for (int j = 0; j < ranges.Count; j++)
+            {
+                if (i == j) continue;
+                var r2 = ranges[j];
+                if (ranges.Where(r2 => r1 != r2).Any(r2 => r2.Low <= r1.Low && r2.High >= r1.High))
+                {
+                    Console.WriteLine("hi");
+                    ranges.Remove(r1);
+                    continue;
+                }
+
+                if (r1.Low <= r2.Low && r2.Low <= r1.High)
+                {
+                    Console.WriteLine("hi3");
+                    r1.High = Math.Max(r1.High, r2.High);
+                    ranges.Remove(r2);
+                    i = -1;
+                    break;
+                }
+                if (r1.Low <= r2.High && r2.High <= r1.High)
+                {
+                    Console.WriteLine("hi4");
+                    r1.Low = Math.Min(r1.Low, r2.Low);
+                    ranges.Remove(r2);
+                    i = -1;
+                    break;
+                }
+            }
+        }
+        // P2
+        long count = 0;
+        ranges.ForEach(range => count += (range.High - range.Low + 1));
         line = sr.ReadLine();
+        Console.WriteLine(count);
         // P1
         var freshCount = 0;
         while (line != null)
@@ -68,14 +104,6 @@
 
             line = sr.ReadLine();
         }
-        // P2
-        // var freshCount = 0;
-        // for (long i = 0; i <= max; i++)
-        // {
-        //     var toCheck = long.Parse(line);
-        //     if (ranges.Any(r => r.Item1 <= toCheck && r.Item2 >= toCheck))
-        //         freshCount++;
-        // }
         Console.WriteLine(freshCount);
         Console.ReadLine();
     }
